6c659041603a7bf13f5437217ff8ca93f867f706
sohamjoshi92 <sohamjoshi92@gmail.com>
2020-06-25 17:03:04 +0530

    UPDATED JWT KEY

--- src/models/user.js
+++ src/models/user.js
@@ -77,7 +77,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
 //methods accessible on instances
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id : user._id.toString() }, process.env.JWT_SECRET_KEY)
+    const token = jwt.sign({ _id : user._id.toString() }, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({ token })
     await user.save()
     return token
